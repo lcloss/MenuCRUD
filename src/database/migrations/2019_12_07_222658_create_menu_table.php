c3c75697bf85ba0a659c5caffa792264163785a2
Luciano Closs <lcloss@gmail.com>
2019-12-31 18:31:36 +0000

    Add menu (group) locations where to place menu items.

---
+++ src/database/migrations/2019_12_07_222658_create_menu_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->enum('position', ['TOP', 'MAIN', 'FOOTER', 'LEFTSIDEBAR', 'RIGHTSIDEBAR'])->default('MAIN');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+}
---
+++ src/database/migrations/2019_12_07_222925_add_menu_to_menu_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMenuToMenuItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('menu_items', function (Blueprint $table) {
+            $table->integer('menu_id')->unsigned()->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('menu_items', function (Blueprint $table) {
+            $table->dropColumn('menu_id');
+        });
+    }
+}
