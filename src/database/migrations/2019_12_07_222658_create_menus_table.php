6a0f3f65fb7c49eb401ed155a2348a976d9507c8
Luciano Closs <lcloss@gmail.com>
2020-01-04 16:16:09 +0000

    Fix migration class CreateMenusTable

--- src/database/migrations/2019_12_07_222658_create_menus_table.php
+++ src/database/migrations/2019_12_07_222658_create_menus_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMenuTable extends Migration
+class CreateMenusTable extends Migration
 {
     /**
      * Run the migrations.
